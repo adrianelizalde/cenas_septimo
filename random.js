2e0a2b64a926b44a14a7e34c6ee374a5cca8c152
Adrian Elizalde <Adrian@MacBook-Pro-de-Adrian.local>
2017-11-16 13:13:15 +0100

    Primer commit

---
+++ random.js
@@ -0,0 +1,9 @@
+var jugadores = ["ignacio","victor","nil","camaleonte","diodoro","rober","lozano","pepe","adrian","ramon","jimeno","carlos"];
+var dado = Math.floor(Math.random() * jugadores.length);
+var mensaje = "<h1>"+jugadores[dado]+"</h1>";
+document.write(mensaje);
+
+var jugadores = ["ignacio","victor","nil","camaleonte","diodoro","rober","lozano","pepe","adrian","ramon","jimeno","carlos"];
+var dado = Math.floor(Math.random() * jugadores.length);
+var mensaje = "<h1>"+jugadores[dado]+"</h1>";
+document.write(mensaje);
