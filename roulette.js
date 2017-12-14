ebe80b0f531e71bda45c520e48cfcf2025b294b3
victormier <victormier@gmail.com>
2017-12-14 14:12:54 +0100

    Add cleaner slot

--- roulette.js
+++ roulette.js
@@ -1,15 +1,49 @@
-var option = {
-  speed : 7,
-  duration : 2,
-  stopImageNumber : 0,
-  startCallback : function() {
-    console.log('start');
-  },
-  slowDownCallback : function() {
-    console.log('slowDown');
-  },
-  stopCallback : function($stopElm) {
-    console.log('stop');
+$(document).ready(function() {
+  function slotRoller(spd, selector) {
+  	var speed = 40;
+  	var firstChild = $(selector).find("li:first-child");
+  		lastChild = $(selector).find("li:last-child");
+
+  	$(selector).animate({
+  		marginTop: "-500px"
+		}, speed + (spd * 30 + spd), "linear", function(){
+			firstChild = $("li:first-child", this);
+			$(this).append(firstChild);
+			$(this).css({ marginTop: "-250px"});
+  	});
   }
-}
-$('div.roulette').roulette(option);
+
+  function rollSlot(slotName){
+  	this.min = 25;
+  	this.max = 60;
+  	this.dice = function(){
+  		var dice = Math.round(Math.random() * (this.max - this.min)) + this.min;
+  		for (var i = 0; i < dice; i++) {
+  			slotRoller(i, slotName);
+  		}
+      var $players = $(slotName).find('li');
+      var names = $players.map(function(i, p) {
+        return $(p).data('name');
+      });
+      var winnerIndex = (dice + 1) % names.length;
+      return winnerIndex;
+  	}
+  }
+
+  var slotRoll1 = new rollSlot('#slot-box1 ul');
+  var slotRoll2 = new rollSlot('#slot-box2 ul');
+
+  $('#slot-button1').on("click", function(){
+  	if( $('#slot-box1 ul').is(':not(:animated)') ){
+  		var winner = slotRoll1.dice();
+      $('#slot-box2 ul li')[winner].remove();
+  	}
+  });
+  $('#slot-button2').on("click", function(){
+    if( $('#slot-box1 ul').is(':not(:animated)') &&
+        $('#slot-box2 ul').is(':not(:animated)') ){
+      // remove member selected on slot1
+      slotRoll2.dice();
+    }
+  });
+});
